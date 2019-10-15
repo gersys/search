c73ff56104841c86ae827f2fc8fa962954d9e978
Cheol-Ho Cho <hoonchcho@gmail.com>
2019-10-15 20:55:47 +0900

    A* final

--- subject_1.py
+++ subject_1.py
@@ -1,121 +1,116 @@
 # [the number of missionaries, cannibals at left of river , and the location of boat (left 1, right 0)]
 
-frontiers=[]
+
 explored=[]
 sol=[]
 
 
-class AStar:
+class AStar: #Astar 클래스 정의
     def __init__(self,prob):
         self.prob=prob
-        self.expand=0
+        self.numberGeneratedNodes=0 #생성된 노드 개수 저장
 
     def solution(self):
+        frontiers = []
         print("A* search start")
-        initial_node = A_star_Node(self.prob.init, None, 0, self.prob.init[0], 0)
+        initial_node = A_star_Node(self.prob.init, None, 0, self.prob.init[0], 0) #처음 시작 노드 생성 ,휴리스틱 왼쪽에 있는 선교사 수 self.prob.init[0]
 
-        frontiers = self.derive_succ(initial_node)
-        explored.append(initial_node.state)
+        frontiers = self.derive_succ(initial_node,frontiers) #시작 노드로 부터 탐색할 노드들 생성
+        explored.append(initial_node.state) #시작 노드를 탐색한 곳으로 분류
 
 
-        while (len(frontiers) != 0):
+        while (len(frontiers) != 0):  #탐색할 곳 중 f_cost가 가장 작은곳을 먼저 탐색
             f_cost = []
             for i in frontiers:
                 f_cost.append(i.f_cost)
-            argmax=-max((x, -i) for i, x in enumerate(f_cost))[1]
+            argmin=-min((x, -i) for i, x in enumerate(f_cost))[1]
 
-            f0 = frontiers.pop(argmax)
+            f0 = frontiers.pop(argmin)
 
             if f0.state == [0, 0, 0]:
                 print("Congratulations! it's a goal : {}".format(f0.state))
                 sol.append(f0)
-                break
+                return f0
 
             explored.append(f0.state)
-            frontiers = frontiers + self.derive_succ(f0)
+            frontiers = frontiers + self.derive_succ(f0,frontiers)
 
-            print("explored:")
-            for i in explored:
-                print(i)
-            print("frontiers:")
-            for i in frontiers:
-                print(i)
+            # for i in frontiers:
+            #     frontier.append(i.state)
 
-            print("--------------------------------")
+            # 잘생성되는지 확인하는 용도의 코드
+            # print("explored:")
+            # for i in explored:
+            #     print(i)
+            # print("frontiers:")
+            # for i in frontiers:
+            #     print(i)
+            #
+            # print("--------------------------------")
 
-        print("solution list")
-        for i in sol:
-            print(i)
-        print("--------------------------------")
-
-        self.sol_path(sol)
-        return self.expand
-
-    def derive_succ(self,input_node):
+    def derive_succ(self,input_node,frontiers): #현재 상태에서 탐색할 노드들 생성하는 함수
         state_list = input_node.state
         succ_list = []
-        l_m = state_list[0]
-        l_c = state_list[1]
-        r_m = 3 - state_list[0]
-        r_c = 3 - state_list[1]
+        l_m = state_list[0] # 왼쪽 선교사수
+        l_c = state_list[1] # 왼쪽 식인종수
+        r_m = 3 - state_list[0] # 오른쪽 선교사수
+        r_c = 3 - state_list[1] # 오른쪽 식인종 수
         boat = state_list[2]
-        if (l_m < l_c and l_m != 0) or (r_m < r_c and r_m != 0):
-            return self.make_Qnode(input_node, succ_list)
+        if (l_m < l_c and l_m != 0) or (r_m < r_c and r_m != 0): #Game over 조건으로 추가로 탐색할 구간을 생성하지 않는다.
+            return self.make_Qnode(input_node, succ_list,frontiers)  #리스트로 생성 후 node로 변경한다.
 
-        if state_list[2] == 1:
+        if state_list[2] == 1: # 배가 왼쪽에 있는 경우에 탐색할 노드 생성
             for i in range(2):
                 if l_m - (i + 1) >= 0:
                     succ_list.append([l_m - (i + 1), l_c, 0])
-                    self.expand = self.expand + 1
+                    self.numberGeneratedNodes = self.numberGeneratedNodes + 1
             for i in range(2):
                 if l_c - (i + 1) >= 0:
                     succ_list.append([l_m, l_c - (i + 1), 0])
-                    self.expand = self.expand + 1
+                    self.numberGeneratedNodes = self.numberGeneratedNodes + 1
             if l_m - 1 >= 0 and l_c - 1 >= 0:
                 succ_list.append([l_m - 1, l_c - 1, 0])
-                self.expand = self.expand + 1
+                self.numberGeneratedNodes = self.numberGeneratedNodes + 1
 
-        elif state_list[2] == 0:
+        elif state_list[2] == 0: # 배가 오른쪽에 있는 경우에 탐색할 노드 생성
             for i in range(2):
                 if r_m - (i + 1) >= 0:
                     succ_list.append([l_m + (i + 1), l_c, 1])
-                    self.expand = self.expand + 1
+                    self.numberGeneratedNodes = self.numberGeneratedNodes + 1
             for i in range(2):
                 if r_c - (i + 1) >= 0:
                     succ_list.append([l_m, l_c + (i + 1), 1])
-                    self.expand = self.expand + 1
+                    self.numberGeneratedNodes = self.numberGeneratedNodes + 1
             if r_m - 1 >= 0 and r_c - 1 >= 0:
                 succ_list.append([l_m + 1, l_c + 1, 1])
-                self.expand = self.expand + 1
+                self.numberGeneratedNodes = self.numberGeneratedNodes + 1
 
-        return self.make_Qnode(input_node, succ_list)
+        return self.make_Qnode(input_node, succ_list,frontiers) #생성된 리스트를 노드로 변경하여 리턴한다.
 
-    def make_Qnode(self, parent_node, suc_list):
+    def make_Qnode(self, parent_node, suc_list,frontiers): #리스트로 되어있는 상태를 노드로 변경하는 함수
         Q = []
         for i in suc_list:
-            suc_node = A_star_Node(i, parent_node, parent_node.g_cost + 1,i[0], parent_node.depth + 1)
-            if suc_node.state in explored:
-                print("jump")
+            suc_node = A_star_Node(i, parent_node, parent_node.g_cost + 1,i[0], parent_node.depth + 1) #휴리스틱 왼쪽에 있는 선교사 수 i[0]
+            frontier=[]
+            for i in frontiers:
+                frontier.append(i.state)
+            if suc_node.state in explored: #탐색한 곳 탐색제외
+                continue
+            elif suc_node.state in frontier: #탐색할 곳에 이미 그 state가 있는 경우 f score를 비교하여 작은걸로 변경해줌
+                for i in frontiers:
+                    if i.state==suc_node.state:
+                        if i.f_cost> suc_node.f_cost:
+                            frontiers.pop(i)
+                            frontiers.append(suc_node)
                 continue
             else:
-                Q.append(suc_node) #replace if there are same state in frontier
+                Q.append(suc_node)
         return Q
-
-    def sol_path(self,sol_list):
-        for sol in sol_list:
-            while (sol.parent != None):
-                print(sol)
-                sol = sol.parent
-            print(sol)
-
-            print("done")
-
 
 class Problem:
     def __init__(self, a, b):
         self.init = a
         self.goal = b
-        #self.model = m
 
     def goal_test(self, state):
         if self.goal == state:
@@ -138,7 +133,6 @@ class A_star_Node:
         self.h_cost=h
         self.f_cost=g+h
         self.depth = d
-        # self.action = a
 
     def __str__(self):
         try:
@@ -148,178 +142,24 @@ class A_star_Node:
 
 
     def solution(self):
-        sol = self.state
-        if self.depth == 0:
-            return sol
-        else:
-            return sol + '<==' + self.parent.solution()
+        p_node = self
+        while (p_node != None):
+            print(str(p_node.state) + " ("+str(p_node.f_cost)+"="+str(p_node.g_cost)+"+"+str(p_node.h_cost)+")")
+            p_node=p_node.parent
+
+        return "solution path return done"
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+
 
 prob=Problem([3,3,1],[0,0,0])
-
-
 astar=AStar(prob)
-expand=astar.solution()
-print("Node generated:{}".format(expand))
-
-
-class Node:
-    def __init__(self,s,p,c,d):
-        self.state = s
-        self.parent = p
-        self.cost = c
-        self.depth = d
-        # self.action = a
-
-    def __str__(self):
-        try:
-            return 'S: ' + str(self.state) +', P: ' + str(self.parent.state) + ', depth = ' + str(self.depth) + ', cost = ' + str(self.cost)
-        except AttributeError:
-            return 'S: ' + str(self.state) + ', P: ' + "None" + ', depth = ' + str(self.depth) + ', cost = ' + str(self.cost)
-
-
-    def solution(self):
-        sol = self.state
-        if self.depth == 0:
-            return sol
-        else:
-            return sol + '<==' + self.parent.solution()
-
-class BFS:
-    def __init__(self,prob):
-        self.prob=prob
-        self.expand = 0
-
-    def make_Qnode(self,parent_node, suc_list):
-        Q = []
-        for i in suc_list:
-            suc_node = Node(i, parent_node, parent_node.cost + 1, parent_node.depth + 1)
-            if suc_node.state in explored:
-                print("jump")
-                continue
-            else:
-                Q.append(suc_node)
-        return Q
-
-    def derive_succ(self,input_node):
-        state_list = input_node.state
-        succ_list = []
-        l_m = state_list[0]
-        l_c = state_list[1]
-        r_m = 3 - state_list[0]
-        r_c = 3 - state_list[1]
-        boat = state_list[2]
-        if (l_m < l_c and l_m != 0) or (r_m < r_c and r_m != 0):
-            return self.make_Qnode(input_node, succ_list)
-
-        if state_list[2] == 1:
-            for i in range(2):
-                if l_m - (i + 1) >= 0:
-                    succ_list.append([l_m - (i + 1), l_c, 0])
-                    self.expand=self.expand+1
-            for i in range(2):
-                if l_c - (i + 1) >= 0:
-                    succ_list.append([l_m, l_c - (i + 1), 0])
-                    self.expand = self.expand + 1
-            if l_m - 1 >= 0 and l_c - 1 >= 0:
-                succ_list.append([l_m - 1, l_c - 1, 0])
-                self.expand = self.expand + 1
-
-        elif state_list[2] == 0:
-            for i in range(2):
-                if r_m - (i + 1) >= 0:
-                    succ_list.append([l_m + (i + 1), l_c, 1])
-                    self.expand = self.expand + 1
-            for i in range(2):
-                if r_c - (i + 1) >= 0:
-                    succ_list.append([l_m, l_c + (i + 1), 1])
-                    self.expand = self.expand + 1
-            if r_m - 1 >= 0 and r_c - 1 >= 0:
-                succ_list.append([l_m + 1, l_c + 1, 1])
-                self.expand = self.expand + 1
-
-        return self.make_Qnode(input_node, succ_list)
-
-    def sol_path(self,sol_list):
-        for sol in sol_list:
-            while (sol.parent != None):
-                print(sol)
-                sol = sol.parent
-            print(sol)
-
-            print("done")
-
-    def solution(self):
-        print("BFS search start")
-        initial_node = Node(self.prob.init, None, 0, 0)
-
-        frontiers = self.derive_succ(initial_node)
-        explored.append(initial_node.state)
-
-        while (len(frontiers) != 0):
-
-            f0 = frontiers.pop(0)
-            if f0.state == [0, 0, 0]:
-                print("Congratulations! it's a goal : {}".format(f0.state))
-                sol.append(f0)
-                break
-
-
-            explored.append(f0.state)
-            frontiers = frontiers + self.derive_succ(f0)
-
-            print("explored:")
-            for i in explored:
-                print(i)
-            print("frontiers:")
-            for i in frontiers:
-                print(i)
-
-            print("--------------------------------")
-
-        print("solution list")
-        for i in sol:
-            print(i)
-        print("--------------------------------")
-
-        self.sol_path(sol)
-        return self.expand
+goalNode=astar.solution()
+print("Node generated:{}".format(astar.numberGeneratedNodes))
+print(goalNode.solution())
 
 
 
 
 
-#bfs=BFS(prob)
-#expand=bfs.solution()
-#print("Node generated:{}".format(expand))
 
-
-
-
-
-# graph={
-#     "[3,3,1]" : ["[3,2,0]","[3,1,0]","[2,3,0]","[1,3,0]","[2,2,0]"],
-#     "[3,2,0]" : ["[3,3,1]"],
-#     "[3,1,0]" : ["[3,3,1]","[3,2,1]"],
-#     "[2,3,0]" : -1, #Game Over
-#     "[1,3,0]" : -1,
-#     "[2,2,0]" : ["[3,2,1]","[2,3,1]","[3,3,1]"],
-#     "[3,2,1]" : ["[3,0,0]","[3,1,0]","[2,2,0]","[1,2,0]","[2,1,0]"],
-#     "[2,3,1]" : -1,
-#     "[3,0,0]" : ["[3,2,1]","[3,1,1]"],
-#     "[1,2,0]" : -1,
-#     "[2,1,0]" : -1,
-#     "[3,1,1]" : ["[3,0,0]","[2,1,0]","[1,1,0]","[2,0,0]"],
-#     "[1,1,0]" : ["[3,1,1]","[2,1,1]","[1,2,1]","[1,3,1]","[2,2,1]"],
-#     "[2,1,1]" : -1,
-#     "[1,2,1]" : -1,
-#     "[1,3,1]" : -1,
-#     "[2,2,1]" : ["[1,1,0]","[1,2,0]","[2,1,0]","[2,0,0]","[0,2,0]"],
-#     "[2,0,0]" : -1,
-#     "[0,2,0]" : ["[2,2,1]","[1,2,1]","[0,3,1]","[1,3,1]"],
-#     "[0,3,1]" : ["[0,2,0]","[0,1,0]"],
-#     "[0,1,0]" : ["[0,3,1]","[1,2,1]","[2,1,1]","[1,1,1]","[0,2,1]"],
-#     "[1,1,1]" : ["[0,1,0]","[1,0,0]","[0,0,0]"],
-#     "[1,0,0]" : -1,
-#     "[0,0,0]" : 0, # goal
-#     "[0,2,1]" : ["[0,1,0]","[0,0,0]"]
-# }
